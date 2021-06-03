88282468b28e0e530cb9e920bbfe5eeec6abf8ba
lizhi <w_lizhi@kingsoft.com>
2021-06-03 15:59:28 +0800

    add

--- main/main.go
+++ main/main.go
@@ -3,32 +3,11 @@ package main
 import (
 	"fmt"
 
-	"github.com/Mr-Cookie-L/algorithm/pkg/tree"
+	"github.com/Mr-Cookie-L/algorithm/pkg/string"
 )
 
 func main() {
-	// {10,6,14,4,8,12,16}
-	root := &tree.TreeNode{
-		Val: 10,
-		Left: &tree.TreeNode{
-			Val: 6,
-			Left: &tree.TreeNode{
-				Val: 4,
-			},
-			Right: &tree.TreeNode{
-				Val: 8,
-			},
-		},
-		Right: &tree.TreeNode{
-			Val: 14,
-			Left: &tree.TreeNode{
-				Val: 12,
-			},
-			Right: &tree.TreeNode{
-				Val: 16,
-			},
-		},
-	}
-	a := tree.PrintFromTopToBottom(root)
-	fmt.Println(a)
+	a := "(()(())"
+	b := string.LongestValidParentheses(a)
+	fmt.Println("b-----------", b)
 }
--- pkg/string/string1.go
+++ pkg/string/string1.go
@@ -19,3 +19,77 @@ func FirstNotRepeatingChar(str string) int {
 	}
 	return -1
 }
+
+/*
+找出一个字符串符合条件的最长子串
+该子串必须有完整的小括号包裹，且子串内没有小括号
+例如：
+输入  (123)(333)(()123123)(1111)
+输出  1111
+
+思路：
+目前还没有找到相关题目的解法，只能使用自己思考出的方法
+	1. 遇到 ( 时，则标记为子串的开头，将之前记录的子串清空
+	2. 遇到 ) 时，判断子串开头是否为 ( ，如果是，则判断当前子串与上一个子串的长度，长则替换，短则清空开头和记录的子串；如果不是则不需要记录
+	3. 直到循环完成，得出的最终子串便为最长子串
+*/
+func LongestSub(input string) string {
+	res := ""
+	start := ""
+	mid := ""
+	for i := 0; i < len(input); i++ {
+		s := string(input[i])
+		switch s {
+		case "(":
+			start = "("
+			mid = ""
+		case ")":
+			if start != "(" {
+				continue
+			}
+			if len(mid) > len(res) {
+				res = mid
+			} else {
+				mid = ""
+				start = ""
+			}
+		default:
+			if start == "(" {
+				mid += s
+			}
+		}
+	}
+	return res
+}
+
+/*
+输入一个只有()的字符串，找出最长的合法子串的长度，即每一个 ( 都能对应一个 )
+例如
+输入 ())(())
+输出 4
+*/
+func LongestValidParentheses(s string) int {
+	res := 0
+	stack := make([]int, len(s)+1)
+	// 边界问题
+	stack[0] = -1
+	top := 0
+	for i, b := range s {
+		str := string(b)
+		if str == "(" {
+			top++
+			stack[top] = i
+		} else {
+			top--
+			if top == -1 {
+				top = 0
+				stack[top] = i
+			} else {
+				if res < i-stack[top] {
+					res = i - stack[top]
+				}
+			}
+		}
+	}
+	return res
+}
